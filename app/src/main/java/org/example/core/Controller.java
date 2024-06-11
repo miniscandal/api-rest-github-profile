97765379946b524c21530cc0edf9215535f60965
Oscar Olsen <user.name>
2024-06-11 03:33:58 -0600

    refactor: Refactored Controller.class for enhanced readability

--- app/src/main/java/org/example/core/Controller.java
+++ app/src/main/java/org/example/core/Controller.java
@@ -11,42 +11,46 @@ import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 
 public abstract class Controller implements ConfigHttpHandler {
-    private static String GITHUB_API_BASE_URI;
-    private String ROUTE_PATH;
-    public final ApiRequestExecuter API_REQUEST;
+    private static String githubApiBaseUri;
+    private String routePath;
+    public ApiRequestExecuter apiRequestExecuter;
     public RequestParameters requestParameters = new RequestParameters();
 
     public abstract byte[] manageResponse(RequestParameters requestParameters);
 
     public Controller(Class<?> modelClass, String githubBaseUri) {
-        API_REQUEST = new ApiRequestExecuter(modelClass);
-        GITHUB_API_BASE_URI = githubBaseUri;
+        apiRequestExecuter = new ApiRequestExecuter(modelClass);
+        githubApiBaseUri = githubBaseUri;
     }
 
     public void setRoutePath(String path) {
-        ROUTE_PATH = path;
+        routePath = path;
     }
 
     public byte[] singleObjectResponse() {
-        String uri = normalizeUriPath();
-        return API_REQUEST.singleObjectResponse(uri);
+        String uri = normalizeUri();
+        return apiRequestExecuter.singleObjectResponse(uri);
     }
 
     public byte[] arrayObjectsResponse() {
-        String uri = normalizeUriPath();
-        return API_REQUEST.arrayObjectResponse(uri);
+        String uri = normalizeUri();
+        return apiRequestExecuter.arrayObjectResponse(uri);
     }
 
     public RequestParameters getRequestParameters(HttpExchange httpExchange) {
         URI uri = httpExchange.getRequestURI();
-        String path = uri.getPath().replace(ROUTE_PATH, "");
-        String words[] = path.split("/");
+        String path = uri.getPath().replace(routePath, "");
+        String wordsPath[] = path.split("/");
         Map<String, String> parameters = requestParameters.getParameters();
         int index = 1;
 
+        if (wordsPath.length - 1 != parameters.size()) {
+            return null;
+        }
+
         for (Map.Entry<String, String> parameter : parameters.entrySet()) {
             String key = parameter.getKey();
-            String value = words[index];
+            String value = wordsPath[index];
             requestParameters.addParameters(key, value);
             index++;
         }
@@ -77,8 +81,13 @@ public abstract class Controller implements ConfigHttpHandler {
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
         requestParameters = getRequestParameters(httpExchange);
-        requestParameters.displayParameters();
-        byte[] response = manageResponse(requestParameters);
+        byte[] response = null;
+        if (requestParameters == null) {
+            String message = "{\"message\": \"URI NOT FOUND\"}";
+            response = message.getBytes();
+        } else {
+            response = manageResponse(requestParameters);
+        }
         sendResponse(httpExchange, response);
     }
 
@@ -87,8 +96,8 @@ public abstract class Controller implements ConfigHttpHandler {
         this.requestParameters = requestParameters;
     }
 
-    private String normalizeUriPath() {
-        String uri = GITHUB_API_BASE_URI;
+    private String normalizeUri() {
+        String uri = githubApiBaseUri;
         Map<String, String> parameters = requestParameters.getParameters();
 
         for (Map.Entry<String, String> parameter : parameters.entrySet()) {
