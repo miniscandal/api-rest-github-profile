6c838d920071c6c25f3515009c8b08c291d2451c
miniscandal <oscar01dev@gmail.com>
2024-08-17 15:48:06 -0600

    feat: validate that the parameters in the property base path match the parameters context path

--- app/src/main/java/org/codeprofile/github/contracts/ApiGitHub.java
+++ app/src/main/java/org/codeprofile/github/contracts/ApiGitHub.java
@@ -1,9 +1,9 @@
-
 package org.codeprofile.github.contracts;
 
 import org.codeprofile.github.services.ApiGitHubClient;
 import org.codeprofile.shared.contracts.Service;
 import org.codeprofile.shared.database.Model;
+import org.codeprofile.shared.http.Controller;
 import org.codeprofile.shared.strategies.ServiceStrategy;
 
 public interface ApiGitHub<T extends Model> extends ServiceStrategy {
@@ -11,8 +11,17 @@ public interface ApiGitHub<T extends Model> extends ServiceStrategy {
 
     Class<T> getModel();
 
+    default void validateExpectedArgumentsBasePath() {
+        for (String element : ((Controller) this).getParameters()) {
+            if (!getBasePath().contains(element)) {
+                throw new IllegalArgumentException("Base path does not contain parameter: " + element);
+            }
+        }
+    }
+
     @Override
     default Service newService() {
+        validateExpectedArgumentsBasePath();
         return new ApiGitHubClient<>(getBasePath(), getModel());
     }
 }
